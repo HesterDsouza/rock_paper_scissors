c6b6ad22e6c6d60d338d941e9eeb7de24d905629
Hester D'Souza <hester.dsouza.08@gmail.com>
2024-04-20 23:03:08 +0530

    Implemented rock paper scissors game. Used loops; counted wins, loses and ties; and displayed score.

---
+++ moves.js
@@ -0,0 +1,102 @@
+//Define the moves
+const moves = ["rock", "paper", "scissors"];
+
+//Accept a move from the user.
+function getUserChoice(){
+    var usermove;
+    do {
+        usermove = prompt("Enter your choice (Rock | Paper | Scissors): ").toLowerCase();
+    } while (!moves.includes(usermove)); 
+    return usermove;
+}
+
+//Funtion to get random move from array of moves for the computer's choice.
+function getComputerChoice(){
+    var random = moves[Math.floor(Math.random() * moves.length)];
+    return random;
+}
+
+//Store the user choice in "playerSelection" variable.
+var playerSelection = getUserChoice();
+
+//Store the computer choice in "computerSelection" variable.
+var computerSelection = getComputerChoice();
+
+//Round Results.
+const winByRock = "You win! Rock crushes scissors."
+const loseByRock = "You lose! Paper beats rock.";
+const winByPaper = "You win! Paper beats rock";
+const loseByPaper = "You lose! Scissors cuts paper.";
+const winByScissors = "You win! Scissors cut paper";
+const loseByScissors = "You lose! Rock crushes scissors";
+const tie = "You both chose the same. You both are tied. Try again!";
+
+//Function to play a round and display results 
+function playRound(playerSelection, computerSelection){    
+    if(playerSelection === "rock" && computerSelection === "scissors"){
+        alert(`Player chose: ${playerSelection} and Computer chose: ${computerSelection}`);
+        alert(winByRock)
+    }
+    else if(playerSelection === "rock" && computerSelection === "paper"){
+        alert(`Player chose: ${playerSelection} and Computer chose: ${computerSelection}`);
+        alert(loseByRock)
+    }
+    else if(playerSelection === "paper" && computerSelection === "rock"){
+        alert(`Player chose: ${playerSelection} and Computer chose: ${computerSelection}`);
+        alert(winByPaper)
+    }
+    else if(playerSelection === "paper" && computerSelection === "scissors"){
+        alert(`Player chose: ${playerSelection} and Computer chose: ${computerSelection}`);
+        alert(loseByPaper)
+    }
+    else if(playerSelection === "scissors" && computerSelection === "paper"){
+        alert(`Player chose: ${playerSelection} and Computer chose: ${computerSelection}`);
+        alert(winByScissors)
+    }
+    else if(playerSelection === "scissors" && computerSelection === "rock"){
+        alert(`Player chose: ${playerSelection} and Computer chose: ${computerSelection}`);
+        alert(loseByScissors)
+    }
+    else if(playerSelection === computerSelection){
+        alert(`Player chose: ${playerSelection} and Computer chose: ${computerSelection}`);
+        alert(tie)
+    }
+    else{
+        alert("Invalid Input!")
+    }
+}
+
+//Function to create the game where rounds are best of five.
+function playGame(){
+    let playerWins = 0;
+    let playerLoses = 0;
+    let ties = 0;
+
+    for(let i=0; i < 5; i++){
+        let playerSelection = getUserChoice();
+        console.log(playerSelection);
+        let computerSelection = getComputerChoice();
+        console.log(computerSelection);
+        playRound(playerSelection, computerSelection);
+        
+        if(playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"){
+                playerWins++;
+        } else if(playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "rock"){
+            playerLoses++;
+        } else{
+            ties++;
+        }
+    }
+
+    if(playerWins > playerLoses){
+        alert("You won the series!")
+    } else if(playerWins < playerLoses) {
+        alert("You lost the series.")
+    } else {
+        alert("Series Tied!")
+    }
+
+    alert(`Player wins: ${playerWins} | Player losses: ${playerLoses}. | Player Ties: ${ties}`);
+}
+
+playGame();
